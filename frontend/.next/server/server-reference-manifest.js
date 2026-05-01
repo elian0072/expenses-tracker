eb523825e1af90238812ee4fe96d28980c9b71a2
Jaime Balbuena <jaime.balbuena@me.com>
2026-05-01 15:19:01 +0200

    chore: remove tracked files that should be gitignored

--- frontend/.next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"nNJhPDQEf35YgbKXVvJKX5g1WTHI60vdYrAg7K0vPEo=\"\n}"
--- frontend/.next/static/chunks/app_(authenticated)_years_[year]_page_tsx_1a1ca7e6._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_@mui_7fc650a4._.js",
-  "static/chunks/_7db3b5ef._.js"
-],
-    source: "dynamic"
-});
